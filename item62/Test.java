55ffa47bc991b7bd47e78c6d874ab4fd0c92f01f
ChoiTaesan <mijosan@naver.com>
2021-09-14 11:05:32 +0900

    study : item 69 예외는 진짜 예외 상황에만 사용하라

---
+++ item62/Test.java
@@ -0,0 +1,9 @@
+package item62;
+
+public class Test {
+    
+    public void test() {
+        System.out.println("test");
+    }
+    
+}
--- item62/ThreadLocal.java
+++ item62/ThreadLocal.java
@@ -1,5 +1,8 @@
 package item62;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // 다른 타입이 적절하다면 문자열 사용을 피하라 !
 
 // 문자열은 다른 값 타입을 대신하기에 적합하지 않다.
@@ -10,7 +13,18 @@ public class ThreadLocal {
     // 이는 단점이 많은 방식이다. 혹여라도 두 요소를 구분해주는 문자 #이 두 요소중 하나에서 쓰였다면 혼란스러운 결과를 초래한다.
     // 각 요소를 개별로 접근하려면 문자열을 파싱해야 해서 느리고, 귀찮고, 오류 가능성도 커진다.
     // 이런 클래스는 보통 private 정적 멤버 클래스로 선언한다.
-    String compoundKey = className + "#" + i.next();
+    public static void main(String[] args) {
+        List<Test> list = new ArrayList<Test>();
+        Test test = new Test();
+
+        list.add(test);
+
+        List list2 = list;
+        list2.add("hello");
+
+        ((Test)list2.get(0)).test();
+    }
+
 
 }
 
